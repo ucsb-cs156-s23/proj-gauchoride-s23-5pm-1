75b0fa962ba81cb8bbd653c223756f14921de423
James Ellenstein <59277482+JamesEllenstein@users.noreply.github.com>
2023-06-01 04:08:31 +0000

    finished compononts-- waiting on backend support for testing

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -1,7 +1,7 @@
-import React from "react";
+import {React, useState} from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable"
-import UserCheckbox from "main/components/Users/UserCheckbox"
+//import UserCheckbox from "main/components/Users/UserCheckbox"
 
 
 import { useBackend } from "main/utils/useBackend";
@@ -15,8 +15,28 @@ const AdminUsersPage = () => {
             { method: "GET", url: "/api/admin/users" },
             []
         );
-/*
-    const { data: allRiders, error: __error, status: __status } =
+        const [isCheckedAll, setIsCheckedAll] = useState(false);
+        const [isCheckedDrivers, setIsCheckedDrivers] = useState(false);
+        const [isCheckedRiders, setIsCheckedRiders] = useState(false);
+        const handleOnChangeAll = () => {
+          setIsCheckedAll(true);
+          setIsCheckedDrivers(false);
+          setIsCheckedRiders(false);
+        };
+        const handleOnChangeDrivers = () => {
+            setIsCheckedDrivers(true);
+            setIsCheckedAll(false);
+            setIsCheckedRiders(false);
+          };
+          const handleOnChangeRiders = () => {
+            setIsCheckedRiders(true);
+            setIsCheckedAll(false);
+            setIsCheckedDrivers(false);
+          };
+
+
+
+    const { data: allRiders, error: __error1, status: __status1 } =
         useBackend(
             // Stryker disable next-line all : don't test internal caching of React Query
             ["/api/admin/riders"],
@@ -25,7 +45,7 @@ const AdminUsersPage = () => {
             []
         );
 
-    const { data: allDrivers, error: _error, status: _status } =
+    const { data: allDrivers, error: _error2, status: _statu2 } =
         useBackend(
             // Stryker disable next-line all : don't test internal caching of React Query
             ["/api/admin/drivers"],
@@ -33,14 +53,42 @@ const AdminUsersPage = () => {
             { method: "GET", url: "/api/admin/drivers" },
             []
         );
-   */     
-    
+        
+    var tableContent = isCheckedAll ? allUsers : (isCheckedDrivers ? allDrivers : allRiders);
 
     return (
         <BasicLayout>
             <h2>Users</h2>
-            <UserCheckbox></UserCheckbox>
-            <UsersTable users={allUsers} />
+            <div className="userType">
+                <input
+                type="checkbox"
+                id="all"
+                name="all"
+                value="All"
+                checked={isCheckedAll}
+                onChange={handleOnChangeAll}
+                />
+                All
+                <input
+                type="checkbox"
+                id="drivers"
+                name="drivers"
+                value="Drivers"
+                checked={isCheckedDrivers}
+                onChange={handleOnChangeDrivers}
+                />
+                Drivers
+                <input
+                type="checkbox"
+                id="riders"
+                name="riders"
+                value="Riders"
+                checked={isCheckedRiders}
+                onChange={handleOnChangeRiders}
+                />
+                Riders
+            </div>
+            <UsersTable users={tableContent} />
         </BasicLayout>
     );
 };
