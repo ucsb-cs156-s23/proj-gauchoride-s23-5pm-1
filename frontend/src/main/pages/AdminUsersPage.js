fc8ea45e4992a9d7a5713674290c6e4fe97e8840
James Ellenstein <59277482+JamesEllenstein@users.noreply.github.com>
2023-06-01 00:09:26 +0000

    je- WIP commit-- pivotiong to fix previous pull request

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable"
+import UserCheckbox from "main/components/Users/UserCheckbox"
+
 
 import { useBackend } from "main/utils/useBackend";
 const AdminUsersPage = () => {
 
-    const { data: users, error: _error, status: _status } =
+    const { data: allUsers, error: _error, status: _status } =
         useBackend(
             // Stryker disable next-line all : don't test internal caching of React Query
             ["/api/admin/users"],
@@ -13,11 +15,32 @@ const AdminUsersPage = () => {
             { method: "GET", url: "/api/admin/users" },
             []
         );
+/*
+    const { data: allRiders, error: __error, status: __status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/admin/riders"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/admin/riders" },
+            []
+        );
+
+    const { data: allDrivers, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/admin/drivers"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/admin/drivers" },
+            []
+        );
+   */     
+    
 
     return (
         <BasicLayout>
             <h2>Users</h2>
-            <UsersTable users={users} />
+            <UserCheckbox></UserCheckbox>
+            <UsersTable users={allUsers} />
         </BasicLayout>
     );
 };
