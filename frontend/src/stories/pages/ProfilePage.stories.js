7945ec947bc20d1417545faf0d268a97eb572336
TG6S85 <joshuageorget@gmail.com>
2023-06-06 15:29:39 -0700

    jt- added storybook for profilepage

---
+++ frontend/src/stories/pages/ProfilePage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HomePage from "main/pages/ProfilePage";
+
+export default {
+    title: 'pages/ProfilePage',
+    component: ProfilePage
+};
+
+const Template = () => <ProfilePage />;
+
+export const Default = Template.bind({});
