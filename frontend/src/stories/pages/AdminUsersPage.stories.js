908ee6092b20df074cee7bd4619979b2cca6358f
James Ellenstein <59277482+JamesEllenstein@users.noreply.github.com>
2023-06-06 21:59:07 +0000

    addedd storybook entry for adminUserPage

---
+++ frontend/src/stories/pages/AdminUsersPage.stories.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import AdminUsersPage from "main/pages/AdminUsersPage";
+
+export default {
+    title: 'pages/AdminUsersPage/AdminUsersPage',
+    component: AdminUsersPage
+};
+
+const Template = () => <AdminUsersPage />;
+
+export const Default = Template.bind({});
+
